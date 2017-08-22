4e546db994889f43caa6191f5e23b6f2b9a2a163
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-08-21 17:11:19 -0700

    Debian - install java - idempotence

--- lib/bash/debian/java7.sh
+++ lib/bash/debian/java7.sh
@@ -2,6 +2,10 @@
 
 # This script depends on the ./java.sh for installation of Oracle java dependencies
 
-echo "java 7 installation"
-apt-get install -y -q oracle-java7-installer
-yes "" | apt-get -f install
+if [ -d /usr/lib/jvm/java-7-oracle ]; then
+    echo "Found java-7-oracle installation"
+else
+    echo "java 7 installation"
+    apt-get install -y -q oracle-java7-installer
+    yes "" | apt-get -f install
+fi
--- lib/bash/debian/java8.sh
+++ lib/bash/debian/java8.sh
@@ -2,6 +2,10 @@
 
 # This script depends on the ./java.sh for installation of Oracle java dependencies
 
-echo "java 8 installation"
-apt-get install -y -q oracle-java8-installer
-yes "" | apt-get -f install
+if [ -d /usr/lib/jvm/java-8-oracle ]; then
+  echo "Found java-8-oracle installation"
+else
+    echo "java 8 installation"
+    apt-get install -y -q oracle-java8-installer
+    yes "" | apt-get -f install
+fi
