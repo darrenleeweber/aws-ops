5dc83ec8aebe187358de806c0766d8d564d47c72
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-08-24 20:49:25 -0700

    Add pssh to the network tools

--- lib/bash/debian/network.sh
+++ lib/bash/debian/network.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
 
-if [ -d /usr/share/doc/wget ]; then
-    echo "Found wget installation"
-else
-    apt-get install -y -qq wget
-fi
+install_package() {
+    package=$1
+    if [ -d "/usr/share/doc/${package}" ]; then
+        echo "Found ${package} installation"
+    else
+        apt-get install -y -qq ${package}
+    fi
+}
 
-if [ -d /usr/share/doc/zip ]; then
-    echo "Found zip installation"
-else
-    apt-get install -y -qq zip
-fi
+install_package ca-certificates
+install_package netcat
+install_package net-tools
+install_package pssh
+install_package tar
+install_package wget
+install_package zip
 
-if [ -d /usr/share/doc/tar ]; then
-    echo "Found tar installation"
-else
-    apt-get install -y -qq tar
-fi
-
-if [ -d /usr/share/doc/netcat ]; then
-    echo "Found netcat installation"
-else
-    apt-get install -y -qq netcat
-fi
-
-if [ -d /usr/share/doc/net-tools ]; then
-    echo "Found net-tools installation"
-else
-    apt-get install -y -qq net-tools
-fi
-
-if [ -d /usr/share/doc/ca-certificates ]; then
-    echo "Found ca-certificates installation"
-else
-    apt-get install -y -qq ca-certificates
-fi
