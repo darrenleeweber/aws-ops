aee00c0c92737c385390ea98b858257987c1872d
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-09-13 10:00:32 -0700

    ServiceManager revisions
    - provide more information for instance termination
    - nodes - compact to remove nil
    - additional state methods
    - specs complete

--- lib/service/service_manager.rb
+++ lib/service/service_manager.rb
@@ -17,32 +17,50 @@ class ServiceManager
   def nodes
     settings.node_names.map do |tag_name|
       AwsHelpers.ec2_find_name_instances(tag_name)
-    end.flatten
+    end.flatten.compact
   end
 
   # All AWS::EC2::Instances for a service that are "alive"
   # @return [Array<Aws::EC2::Instance>]
   def nodes_alive
-    nodes.reject { |i| i.state.name.to_s == 'terminated' }
+    nodes.reject { |i| i.state.name == 'terminated' }
+  end
+
+  # All AWS::EC2::Instances for a service that are "pending"
+  # @return [Array<Aws::EC2::Instance>]
+  def nodes_pending
+    nodes.select { |i| i.state.name == 'pending' }
   end
 
   # All AWS::EC2::Instances for a service that are "running"
   # @return [Array<Aws::EC2::Instance>]
   def nodes_running
-    nodes.select { |i| i.state.name.to_s == 'running' }
+    nodes.select { |i| i.state.name == 'running' }
+  end
+
+  # All AWS::EC2::Instances for a service that are "shutting_down"
+  # @return [Array<Aws::EC2::Instance>]
+  def nodes_shutting_down
+    nodes.select { |i| i.state.name == 'shutting-down' }
   end
 
   # All AWS::EC2::Instances for a service that are "stopped"
   # @return [Array<Aws::EC2::Instance>]
   def nodes_stopped
-    nodes.select { |i| i.state.name.to_s == 'stopped' }
+    nodes.select { |i| i.state.name == 'stopped' }
+  end
+
+  # All AWS::EC2::Instances for a service that are "stopping"
+  # @return [Array<Aws::EC2::Instance>]
+  def nodes_stopping
+    nodes.select { |i| i.state.name == 'stopping' }
   end
 
   # All AWS::EC2::Instances for a service that are "terminated"
   # - terminated instances are accessible for a short period
   # @return [Array<Aws::EC2::Instance>]
   def nodes_terminated
-    nodes.select { |i| i.state.name.to_s == 'terminated' }
+    nodes.select { |i| i.state.name == 'terminated' }
   end
 
   # The tag 'Name' of AWS::EC2::Instances in a service
@@ -51,61 +69,47 @@ class ServiceManager
     nodes.map { |i| node_name(i) }
   end
 
-  # The tag 'Name' of AWS::EC2::Instances in a service
+  # The tag 'Name' of an AWS::EC2::Instance in a service
   # @return [String]
   def node_name(node)
     AwsHelpers.ec2_instance_tag_name(node)
   end
 
+  # The Settings config for a node
+  # @param node [AWS::EC2::Instance]
+  # @return [Config::Options]
+  def node_config(node)
+    settings.find_by_name(node_name(node))
+  end
+
   # Assumes the node name is unique for nodes that are not terminated
-  # @return [Aws::EC2::Instance]
+  # @return [Aws::EC2::Instance | nil]
   def find_node_by_name(name)
-    nodes = nodes_alive.select { |i| AwsHelpers.ec2_instance_tag_name?(i, name) }
-    raise 'Found too many nodes' if nodes.length > 1
-    raise "Not Found: #{name}" if nodes.empty?
-    nodes.first
+    nodes_alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, name) }
   end
 
   # Find and describe all nodes
-  # @return [nil] prints information
+  # @return [Array<String>] collects information
   def describe_nodes
-    nodes.each { |i| AwsHelpers.ec2_instance_describe(i) }
+    nodes.collect { |i| AwsHelpers.ec2_instance_describe(i) }
   end
 
   # Create /etc/hosts data
   # @param public [Boolean]
   def etc_hosts(public = true)
-    alive = nodes_alive
-    settings.nodes.map do |n|
-      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, n.tag_name) }
-      next if inst.nil?
+    nodes_alive.map do |inst|
       hosts = AwsHelpers.ec2_instance_etc_hosts(inst, public)
-      hosts.sub!('{HOST}', n.tag_name)
+      hosts.sub!('{HOST}', node_name(inst))
     end
   end
 
   # Create entries for ~/.ssh/config
   def ssh_config(public = true)
-    alive = nodes_alive
-    settings.nodes.map do |n|
-      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, n.tag_name) }
-      next if inst.nil?
-      hosts = AwsHelpers.ec2_instance_ssh_config(inst, public)
-      hosts.sub!('{HOST}', n.tag_name)
-      hosts.sub!('{USER}', n.user)
-    end
-  end
-
-  # Create nodes
-  def create_nodes
-    alive = nodes_alive
-    settings.nodes.each do |params|
-      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, params.tag_name) }
-      if inst.nil?
-        AwsHelpers.ec2_create params
-      else
-        puts "Found existing active node named: #{params.tag_name}"
-      end
+    nodes_alive.map do |node|
+      config = settings.find_by_name(node_name(node))
+      hosts = AwsHelpers.ec2_instance_ssh_config(node, public)
+      hosts.sub!('{HOST}', config.tag_name)
+      hosts.sub!('{USER}', config.user)
     end
   end
 
@@ -113,79 +117,99 @@ class ServiceManager
   # Attempts to be idempotent, but if details of settings change,
   # it cannot inspect everything to detect updates or node replacements.
   # To replace nodes and change their settings, terminate them and recreate them.
-  def create_node(params)
-    inst = find_node_by_name(params.tag_name)
-    if inst.nil?
-      AwsHelpers.ec2_create params
-    elsif inst.state.name.to_s == 'terminated'
-      AwsHelpers.ec2_create params
+  # @param config [Config::Options]
+  def create_node(config)
+    inst = find_node_by_name(config.tag_name)
+    if inst.nil? || inst.state.name == 'terminated'
+      AwsHelpers.ec2_create config
     else
-      puts "Found existing node named: #{params.tag_name}"
+      puts "Found existing node named: #{config.tag_name}"
+    end
+  end
+
+  # Create nodes
+  def create_nodes
+    alive = nodes_alive
+    settings.nodes.each do |config|
+      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, config.tag_name) }
+      if inst.nil?
+        AwsHelpers.ec2_create config
+      else
+        puts "Found existing active node named: #{config.tag_name}"
+      end
     end
   end
 
   # Reboot node
-  def reboot_node(params)
-    alive = nodes_alive
-    inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, params.tag_name) }
+  # @param config [Config::Options]
+  def reboot_node(config)
+    inst = find_node_by_name(config.tag_name)
     return if inst.nil?
-    puts "Rebooting active node named: #{params.tag_name}"
+    puts "Rebooting active node named: #{config.tag_name}"
     AwsHelpers.ec2_reboot_instance(inst.id)
   end
 
   # Reboot nodes
   def reboot_nodes
     alive = nodes_alive
-    settings.nodes.each do |params|
-      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, params.tag_name) }
+    settings.nodes.each do |config|
+      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, config.tag_name) }
       next if inst.nil?
-      puts "Rebooting active node named: #{params.tag_name}"
+      puts "Rebooting active node named: #{config.tag_name}"
       AwsHelpers.ec2_reboot_instance(inst.id)
     end
   end
 
   # Stop node
-  def stop_node(params)
-    alive = nodes_alive
-    inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, params.tag_name) }
+  # @param config [Config::Options]
+  def stop_node(config)
+    inst = find_node_by_name(config.tag_name)
     return if inst.nil?
-    puts "Stoping active node named: #{params.tag_name}"
+    puts "Stopping active node named: #{config.tag_name}"
     AwsHelpers.ec2_stop_instance(inst.id)
   end
 
   # Stop nodes
   def stop_nodes
     alive = nodes_alive
-    settings.nodes.each do |params|
-      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, params.tag_name) }
+    settings.nodes.each do |config|
+      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, config.tag_name) }
       next if inst.nil?
-      puts "Stoping active node named: #{params.tag_name}"
+      puts "Stopping active node named: #{config.tag_name}"
       AwsHelpers.ec2_stop_instance(inst.id)
+    end
+  end
+
+  # Terminate node
+  # Requests confirmations for destructive actions
+  # @param config [Config::Options]
+  def terminate_node(config)
+    inst = find_node_by_name(config.tag_name)
+    if inst.nil?
+      puts "Could not find #{config.tag_name} node"
+    else
+      return unless confirmation?("Terminate: #{node_status(inst)}")
+      AwsHelpers.ec2_terminate_instance(inst.id)
     end
   end
 
   # Terminate nodes
   # Requests confirmations for destructive actions
   def terminate_nodes
-    return unless confirmation?("DANGER: terminating #{service} can impact other services")
-    all = confirmation?("DANGER: terminate all #{service} nodes")
     alive = nodes_alive
-    settings.nodes.each do |params|
-      inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, params.tag_name) }
-      next if inst.nil?
-      next unless all || confirmation?("DANGER: terminate #{params.tag_name}")
-      AwsHelpers.ec2_terminate_instance(inst.id)
+    if alive.empty?
+      puts "There are no #{service} nodes alive"
+    else
+      return unless confirmation?("DANGER: terminating #{service} might impact other services")
+      alive.each { |i| puts "Node alive: #{node_status(i)}" }
+      all = confirmation?("DANGER: terminate all #{service} nodes")
+      settings.nodes.each do |config|
+        inst = alive.find { |i| AwsHelpers.ec2_instance_tag_name?(i, config.tag_name) }
+        next if inst.nil?
+        next unless all || confirmation?("DANGER: terminate #{node_status(inst)}")
+        AwsHelpers.ec2_terminate_instance(inst.id)
+      end
     end
-  end
-
-  # Terminate node
-  # Attempts to be idempotent
-  # Requests confirmations for destructive actions using `Capfile#confirmation?` (find a better pattern)
-  def terminate_node(params)
-    inst = find_node_by_name(params.tag_name)
-    return if inst.nil?
-    return unless confirmation?("Terminate: #{params.tag_name}")
-    AwsHelpers.ec2_terminate_instance(inst.id)
   end
 
   private
@@ -196,6 +220,10 @@ class ServiceManager
     cli = HighLine.new
     confirm = cli.ask("#{msg}; do it? [y/n] ") { |yn| yn.limit = 1, yn.validate = /[yn]/i }
     confirm.downcase == 'y' # rubocop:disable Performance/Casecmp
+  end
+
+  def node_status(inst)
+    "#{AwsHelpers.ec2_instance_tag_name(inst)} (#{inst.id}, #{inst.state.name})"
   end
 
 end
