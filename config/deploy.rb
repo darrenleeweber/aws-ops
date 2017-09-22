93981d9c2ad820518d3e89fa46f934977e808d75
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-09-22 14:55:20 -0700

    config/deploy - revise linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,8 @@ set :deploy_to, '~/aws-ops'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets'
+append :linked_dirs, 'log/debian', 'log/redhat'
+append :linked_dirs, 'tmp/pids', 'tmp/cache', 'tmp/sockets'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
