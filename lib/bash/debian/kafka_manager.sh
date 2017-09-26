18552201bed819dba3919e460d1291c71b975fc5
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-09-26 09:47:58 -0700

    KafkaManager - installation revised

--- lib/bash/debian/kafka_manager.sh
+++ lib/bash/debian/kafka_manager.sh
@@ -23,12 +23,14 @@ After=network.target
 WantedBy=multi-user.target
 
 [Service]
-ExecStart=/usr/bin/kafka-manager
+ExecStart=/usr/bin/kafka-manager -Dpidfile.path=/var/run/kafka-manager.pid -Dconfig.file=/etc/kafka-manager/application.conf -Dlogger.file=/etc/kafka-manager/logger.xml
 Type=simple
 Restart=always
 EOF
 
 sudo mv /tmp/kafka-manager.service /lib/systemd/system/
+sudo chown root:root /lib/systemd/system/kafka-manager.service
+sudo chmod 644       /lib/systemd/system/kafka-manager.service
 sudo systemctl enable kafka-manager.service
 
 # TODO: Create 'kafka' user/group to run the service
