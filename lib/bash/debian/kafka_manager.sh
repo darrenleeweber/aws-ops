390050c0ac61e45b6d46785da5d14d19d48c3ba9
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-09-26 07:59:55 -0700

    KafkaManager - installation can enable systemd

--- lib/bash/debian/kafka_manager.sh
+++ lib/bash/debian/kafka_manager.sh
@@ -7,7 +7,29 @@ fi
 
 curl -s https://packagecloud.io/install/repositories/spuder/kafka-manager/script.deb.sh | sudo bash
 
-sudo apt install kafka-manager
+sudo apt-get install kafka-manager
+
+
+#---
+# Enable systemd
+# https://github.com/yahoo/kafka-manager/issues/373
+
+cat > /tmp/kafka-manager.service <<EOF
+[Unit]
+Description=Kafka Manager
+After=network.target
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+ExecStart=/usr/bin/kafka-manager
+Type=simple
+Restart=always
+EOF
+
+sudo mv /tmp/kafka-manager.service /lib/systemd/system/
+sudo systemctl enable kafka-manager.service
 
 exit
 
